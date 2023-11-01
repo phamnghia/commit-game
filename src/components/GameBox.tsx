ce9a5ae0a138b2d48cdb712d9c75549b87bf16b5
PhamNghia <nghiapd92@gmail.com>
2023-11-01 21:36:23 +0700

    init

---
+++ src/components/GameBox.tsx
@@ -0,0 +1,83 @@
+import { Button, Divider, Drawer, Modal } from "rsuite";
+import questions from "../data/questions";
+import { useEffect, useState } from "react";
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+
+
+export default function GameBox() {
+  const [selected, setSelected] = useState<number>();
+  const [isViewingQuestion, setIsViewingQuestion] = useState<boolean>(false);
+
+  const [answered, setAnswered] = useState<number[]>([]);
+  const [itv, setItv] = useState<any>();
+
+  function getRandom(list: any[]) {
+    return list[Math.floor((Math.random()*list.length))];
+  }
+
+  const toggleDraw = () => {
+    if (!itv) {
+      const itv = setInterval(() => {
+        const s = getRandom([0, 1, 2, 3, 4, 5].filter(i => !answered.includes(i)));
+        setSelected(s);
+      }, 100);
+
+      setItv(itv);
+    } else {
+      clearInterval(itv);
+      setItv(undefined);
+    }
+  }
+
+  const viewQuestion = (index: number) => {
+    setAnswered([...answered, index]);
+
+    if (selected === index) {
+      setIsViewingQuestion(true);
+    }
+  }
+
+  return <div className="p-5">
+    <div className="flex gap-3 mb-5 items-center">
+      <div className="text-xl flex-grow font-semibold">Chặng I: Khởi động</div>
+      <Button appearance="primary" onClick={() => toggleDraw()}>{itv ? "Dừng" : "Quay"}</Button>
+    </div>
+
+    <div className="grid grid-cols-3 gap-3">
+      { questions.map((q, index) => <div
+        key={index}
+        onClick={() => viewQuestion(index)}
+        className={`w-full h-96 border rounded-md p-3 text-9xl ${answered.includes(index) ? "bg-gray-100" : "bg-cyan-100"} flex flex-row items-center ${selected === index ? "border-8 border-blue-400" : ""}`}
+      >
+        <div className="text-center w-full font-semibold">{index + 1}</div>
+      </div>) }
+    </div>
+
+    <Drawer size="full" placement="bottom" open={isViewingQuestion} onClose={() => setIsViewingQuestion(false)}>
+      <Drawer.Body>
+        <div className="w-full h-full">
+          <div className="font-semibold mb-3 text-lg p-3 border bg-white rounded-md">{selected !== undefined && questions[selected].ask}</div>
+          
+          <div className="grid grid-cols-2 gap-3">
+            { selected !== undefined && questions[selected].answers.map((answer, i) => <div key={i} className="bg-white border rounded-md p-4">
+              {answer}
+            </div>) }
+          </div>
+
+          <div className="w-full p-3 mt-2 text-lg flex flex-col items-center">
+            <CountdownCircleTimer
+              isPlaying
+              size={80}
+              duration={45}
+              colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+              colorsTime={[7, 5, 2, 0]}
+              onComplete={() => setIsViewingQuestion(false)}
+            >
+              {({ remainingTime }) => remainingTime}
+            </CountdownCircleTimer>
+          </div>
+        </div>
+      </Drawer.Body>
+    </Drawer>
+  </div>
+}
---
+++ src/data/questions.ts
@@ -0,0 +1,58 @@
+const questions = [
+  {
+    ask: "Địa điểm nào dưới đây không phải là trụ sở của CTS?",
+    answers: [
+      "A. Khu đô thị mới Định Công",
+      "B. HAREC Building: 4B Láng Hạ, Đống Đa, Hà Nội",
+      "C. TTC Building, 19 Duy tân, Cầu Giấy, Hà Nội",
+      "D.  IC Building, 82 Duy Tân, Cầu Giấy, Hà Nội"
+    ]
+  },
+  {
+    ask: "Tên đầy đủ của CSF là gì?",
+    answers: [
+      "A. COMIT Success Factor",
+      "B. COMIT Smart Function",
+      "C. COMIT Smart Factory",
+      "D. COMIT Success Function",
+    ]
+  },
+  {
+    ask: "Anh Hồng Sơn và Đức Long đã tham dự “Hội nghị Thượng đỉnh về Thành phố Thông minh tại Đài Loan” vào năm nào?",
+    answers: [
+      "A. 2016",
+      "B. 2017",
+      "C. 2018",
+      "D. 2019",
+    ]
+  },
+  {
+    ask: "COMIT đã thành lập nhóm Task Force vào tháng 9/2012. Mục đích của nhóm này là gì?",
+    answers: [
+      "A. Phản ứng nhanh trước các tình huống",
+      "B. Tìm kiếm các đối tác mới",
+      "C. Kêu gọi đầu tư để đẩy mạnh tiềm lực tài chính",
+      "D. Tập trung tìm kiếm cơ hội phát triển kinh doanh mới",
+    ]
+  },
+  {
+    ask: "Một nguyên tắc của COMIT trong việc tiến hành các khóa đào tạo vào thời kỳ đầu mới thành lập là gì?",
+    answers: [
+      "A. Kết hợp đào tạo xa Hà Nội với hoạt động Team Building",
+      "B. Đào tạo xa Hà Nội",
+      "C. Kết hợp đào tạo với hoạt động Team Building",
+      "D. Các đáp án trên đều sai",
+    ]
+  },
+  {
+    ask: "Suy thoái kinh tế vĩ mô và ngành viễn thông là cơn sóng dữ thứ mấy mà COMIT phải đối mặt?",
+    answers: [
+      "A. 1",
+      "B. 2",
+      "C. 3",
+      "D. 4",
+    ]
+  }
+]
+
+export default questions;
