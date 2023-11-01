e256b690feb8a9cf8b6d645dac5b6e6d8b12a62f
PhamNghia <nghiapd92@gmail.com>
2023-11-01 22:11:59 +0700

    ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "",
+  adapter: node({
+    mode: "standalone"
+  }),
   integrations: [react(), tailwind()]
 });
